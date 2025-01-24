0a93c4ff1a28e502860bbea648df29a7c4615cf8
Frost Flower Fairy <139527061+FrostFlowerFairy@users.noreply.github.com>
2025-01-24 12:43:15 +0100

    Update routes.jsx

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,11 +1,12 @@
 import { Navigate, useRoutes } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 import DashboardLayout from "./layouts/dashboard";
 import SimpleLayout from "./layouts/simple";
+import DashboardPage from "./pages/dashboard/DashboardPage";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
 import Page404 from "./pages/Page404";
-import DashboardPage from "./pages/dashboard/DashboardPage";
 
 export default function Router() {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -13,34 +14,24 @@ export default function Router() {
   const routes = useRoutes([
     {
       path: "/",
-      element: <Navigate to="/dashboard" replace />,
-    },
-    {
-      path: "/dashboard",
       element: isAuthenticated ? <DashboardLayout /> : <Navigate to="/login" />,
       children: [
-        { element: <Navigate to="/dashboard/app" />, index: true },
-        { path: "app", element: <DashboardPage /> },
+        { element: <Navigate to="/dashboard" />, index: true },
+        { path: "dashboard", element: <DashboardPage /> },
       ],
     },
     {
       path: "login",
-      element: isAuthenticated ? <Navigate to="/dashboard" /> : <LoginPage />,
+      element: isAuthenticated ? <Navigate to="/" /> : <LoginPage />,
     },
     {
       path: "register",
-      element: isAuthenticated ? (
-        <Navigate to="/dashboard" />
-      ) : (
-        <RegisterPage />
-      ),
+      element: isAuthenticated ? <Navigate to="/" /> : <RegisterPage />,
     },
     {
+      path: "404",
       element: isAuthenticated ? <SimpleLayout /> : <Navigate to="/login" />,
-      children: [
-        { path: "404", element: <Page404 /> },
-        { path: "*", element: <Navigate to="/404" /> },
-      ],
+      children: [{ element: <Page404 />, index: true }],
     },
     {
       path: "*",
